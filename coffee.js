28b7c06fc6bb375924081a3d0d8296a1c043bfc1
ajanakos <ajanakos@ucsc.edu>
2016-05-18 10:41:01 -0700

    initial file push

---
+++ coffee.js
@@ -0,0 +1,20 @@
+
+            
+svg.append("text")
+    .attr("x", 0)             
+    .attr("y", height + margin.top + 10)  
+    .attr("width", )
+    .style("fill", "steelblue")         
+    .on("click", function(){
+        
+    }
+
+    
+//        // Determine if current line is visible
+//        var active   = blueLine.active ? false : true,
+//          newOpacity = active ? 0 : 1;
+//        // Hide or show the elements
+//        d3.select("#blueLine").style("opacity", newOpacity);
+//        d3.select("#blueAxis").style("opacity", newOpacity);
+//        // Update whether or not the elements are active
+//        blueLine.active = active;
