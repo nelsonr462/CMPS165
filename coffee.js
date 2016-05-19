115499a43cd5c93172661e84c05f671ff9d469cc
ajanakos <ajanakos@ucsc.edu>
2016-05-18 21:06:32 -0700

    can now set coffee object for bean, roast, method... also added pictures

--- coffee.js
+++ coffee.js
@@ -1,1 +1,28 @@
 console.log(data);
+
+var coffeeSet = {
+    
+    // Attributes the user must select
+    bean: "",
+    roast: "",
+    method: "",
+    mods: [,]
+}
+
+var coffee = coffeeSet;
+
+function setBean(data) {
+    coffee.bean = data.toString();
+    alert(coffee.bean);
+}
+
+function setRoast(data) {
+    coffee.roast = data.toString();
+    alert(coffee.roast);
+}
+
+function setMethod(data) {
+    coffee.method = data.toString();
+    alert(coffee.method);
+}
+
