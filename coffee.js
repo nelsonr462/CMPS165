dd495f4e5f46041b3b3720996fa50969bc7d979a
ajanakos <ajanakos@ucsc.edu>
2016-05-19 17:57:30 -0700

    coffe stats wokring

--- coffee.js
+++ coffee.js
@@ -22,30 +22,39 @@ var coffee = coffeeSet;
 $(".bean").click(function(){
     coffee.bean = $(this).attr('data')
     console.log(coffee.bean)
+    calc(coffee)
 })
 
 // Set method
 $(".method").click(function(){
     coffee.method = $(this).attr('data')
     console.log(coffee.method)
+    calc(coffee)
+
 })
 
 // Set roast
 $(".roast").click(function(){
     coffee.roast = $(this).attr('data')
     console.log(coffee.roast)
+    calc(coffee)
+
 })
 
 // Set milk
 $(".milk").click(function(){
     coffee.milk = $(this).attr('data')
     console.log(coffee.milk)
+    calc(coffee)
+
 })
 
 // Set sweetener
 $(".sweetener").click(function(){
     coffee.sweetener = $(this).attr('data')
     console.log(coffee.sweetener)
+    calc(coffee)
+
 })
 
 // Calculate calories, caffeine, flavor and price of a given cup of coffee
@@ -54,33 +63,36 @@ function calc(coffeeVal) {
     // Initialize base flavor
     flavor = 0.5
     
+    // Store name of method for calculation
+    var method = ""
+    
     // Size in ounces of the cup of coffee
-    var size = 0;
+    var size = 0
     
     // Grams in a cup
-    var gramsPerCup = 0;
+    var gramsPerCup = 0
     
     // Caffeine variables
-    var caffeine_mg = 0;
-    var base_caffeine_scalar = 0;
-    var caffeine_scalar = 0;
+    var caffeine_mg = 0
+    var base_caffeine_scalar = 0
+    var caffeine_scalar = 0
     
     // Calorie variables
-    var calorie_ounce_method = 0;
-    var calorie_ounce_milk = 0;
-    var calorie_ounce_sweetener = 0;
+    var calorie_ounce_method = 0
+    var calorie_ounce_milk = 0
+    var calorie_ounce_sweetener = 0
     
     // Flavor variables
-    var bean_flavor = 0;
-    var method_flavor = 0;
-    var roast_flavor = 0;
-    var milk_flavor = 0;
-    var sweetener_flavor = 0;
+    var bean_flavor = 0
+    var method_flavor = 0
+    var roast_flavor = 0
+    var milk_flavor = 0
+    var sweetener_flavor = 0
     
     // Price variables
-    var bean_price = 0;
-    var milk_price = 0;
-    var sweetener_price = 0;
+    var bean_price = 0
+    var milk_price = 0
+    var sweetener_price = 0
     
     /*
     We loop though all sub-objects of the main data object.
@@ -95,7 +107,7 @@ function calc(coffeeVal) {
         if (!data.hasOwnProperty(key)) continue;
         
         // Set new object to data property
-        var obj = data[key];
+        var obj = data[key]
         
         for (var prop in obj) {
             
@@ -105,78 +117,123 @@ function calc(coffeeVal) {
             // Adjust variables based on bean type
             if (obj[prop]["name"] == coffee.bean) {
                 
-                console.log(obj[prop]["name"]);
+                console.log(obj[prop]["name"])
                 
                 // Set needed variables
-                base_caffeine_scalar = obj[prop]["base_caffeine_scalar"];
-                bean_flavor = obj[prop]["bean_flavor"];
-                bean_price = obj[prop]["bean_price"];
+                base_caffeine_scalar = obj[prop]["base_caffeine_scalar"]
+                bean_flavor = obj[prop]["bean_flavor"]
+                bean_price = obj[prop]["bean_price"]
             }
                 
             // Adjust variables based on roast type
             if (obj[prop]["name"] == coffee.roast) {
                 
-                console.log(obj[prop]["name"]);
+                console.log(obj[prop]["name"])
                 
                 // Set needed variables
-                caffeine_scalar = obj[prop]["caffeine_scalar"];
-                roast_flavor = obj[prop]["roast_flavor"];
+                caffeine_scalar = obj[prop]["caffeine_scalar"]
+                roast_flavor = obj[prop]["roast_flavor"]
             }
             
             // Adjust variables based on method type
             if (obj[prop]["name"] == coffee.method) {
                 
-                console.log(obj[prop]["name"]);
+                console.log(obj[prop]["name"])
+                
+                // Set methood name
+                method = obj[prop]["name"]
                 
                 // Set needed variables
-                caffeine_mg = obj[prop]["caffeine_mg"];
-                size = obj[prop]["size"];
-                calorie_ounce_method = obj[prop]["calorie_ounce_method"];
-                method_flavor = obj[prop]["method_flavor"];
-                gramsPerCup = obj[prop]["gramsPerCup"];
+                caffeine_mg = obj[prop]["caffeine_mg"]
+                size = obj[prop]["size"]
+                calorie_ounce_method = obj[prop]["calorie_ounce_method"]
+                method_flavor = obj[prop]["method_flavor"]
+                gramsPerCup = obj[prop]["gramsPerCup"]
             }
             
             // Adjust variables based on milk type
             if (obj[prop]["name"] == coffee.milk) {
                 
-                console.log(obj[prop]["name"]);
+                console.log(obj[prop]["name"])
                 
                 // Set needed variables
-                calorie_ounce_milk = obj[prop]["calorie_ounce_milk"];
-                milk_flavor = obj[prop]["milk_flavor"];
-                milk_price = obj[prop]["milk_price"];    
+                calorie_ounce_milk = obj[prop]["calorie_ounce_milk"]
+                milk_flavor = obj[prop]["milk_flavor"]
+                milk_price = obj[prop]["milk_price"]   
             }
             
             // Adjust variables based on sweetener type
             if (obj[prop]["name"] == coffee.sweetener) {
                 
-                console.log(obj[prop]["name"]);
+                console.log(obj[prop]["name"])
                 
                 // Set needed variables
-                calorie_ounce_sweetener = obj[prop]["calorie_ounce_sweetener"];
-                sweetener_flavor = obj[prop]["sweetener_flavor"];
-                sweetener_price = obj[prop]["sweetener_price"];
+                calorie_ounce_sweetener = obj[prop]["calorie_ounce_sweetener"]
+                sweetener_flavor = obj[prop]["sweetener_flavor"]
+                sweetener_price = obj[prop]["sweetener_price"]
             }
         }
     }
     // All values have been set, begin coffee calculations
     
-    console.log("Size: "+size);
-    console.log("gramsPerCup: "+gramsPerCup);
-    console.log("caffeine_mg: "+caffeine_mg);
-    console.log("base_caffeine_scalar: "+base_caffeine_scalar);
-    console.log("caffeine_scalar: "+caffeine_scalar);
-    console.log("calorie_ounce_method: "+calorie_ounce_method);
-    console.log("calorie_ounce_milk: "+calorie_ounce_milk);
-    console.log("calorie_ounce_sweetener: "+calorie_ounce_sweetener);
-    console.log("bean_flavor: "+bean_flavor);
-    console.log("method_flavor: "+method_flavor);
-    console.log("roast_flavor: "+roast_flavor);
-    console.log("milk_flavor: "+milk_flavor);
-    console.log("sweetener_flavor: "+sweetener_flavor);
-    console.log("bean_price: "+bean_price);
-    console.log("milk_price: "+milk_price);
-    console.log("sweetener_price: "+sweetener_price);   
+    // Total caffeine in mg
+    caffeine = (base_caffeine_scalar + caffeine_scalar) * (caffeine_mg)
+    
+    // Turkish and espresso both have different volumes requiring different 
+    // calorie and price calculations
+    if (method == "Turkish" || method == "Espresso") {
+        
+        // Calculate calories for Turkish/Espresso methods
+        calories = 
+            (calorie_ounce_method * size) + 
+            (calorie_ounce_milk * size) + 
+            (calorie_ounce_sweetener * (1/6))
+        
+        // Calculate price for Turkish/Espresso methods
+        price =
+            (bean_price * gramsPerCup) +
+            (milk_price * size) +
+            (sweetener_price * (1/6))
+    }
+    else {
+        
+        // Calculate calories for Turkish/Espresso methods
+        calories = 
+            (calorie_ounce_method * size) +
+            (calorie_ounce_milk * 2) +
+            (calorie_ounce_sweetener * 2)
+        
+        price =
+            (bean_price * gramsPerCup) +
+            (milk_price * 2) +
+            (sweetener_price * 2)
+    }
+    
+    // Flavor
+    flavor = bean_flavor + method_flavor + roast_flavor + milk_flavor + sweetener_flavor
+    
+    console.log("Size: "+size)
+    console.log("gramsPerCup: "+gramsPerCup)
+    console.log("Method: "+method)
+    console.log("caffeine_mg: "+caffeine_mg)
+    console.log("base_caffeine_scalar: "+base_caffeine_scalar)
+    console.log("caffeine_scalar: "+caffeine_scalar)
+    console.log("calorie_ounce_method: "+calorie_ounce_method)
+    console.log("calorie_ounce_milk: "+calorie_ounce_milk)
+    console.log("calorie_ounce_sweetener: "+calorie_ounce_sweetener)
+    console.log("bean_flavor: "+bean_flavor)
+    console.log("method_flavor: "+method_flavor)
+    console.log("roast_flavor: "+roast_flavor)
+    console.log("milk_flavor: "+milk_flavor)
+    console.log("sweetener_flavor: "+sweetener_flavor)
+    console.log("bean_price: "+bean_price)
+    console.log("milk_price: "+milk_price)
+    console.log("sweetener_price: "+sweetener_price)
+    console.log("BEGIN COFFEE STATS")
+    console.log("Caffeine: "+caffeine+" mg")
+    console.log("Calories: "+calories)
+    console.log("Flavor: "+ flavor)
+    console.log("Price: "+price)
 }
 
-calc(coffee);
+calc(coffee)
