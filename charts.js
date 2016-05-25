bf1d2e1778670b5d9a1ce6c9cf99a4ba855cf1c5
ajanakos <ajanakos@ucsc.edu>
2016-05-25 10:12:19 -0700

    separated js files for clarity sake

--- charts.js
+++ charts.js
@@ -1,7 +1,13 @@
-$(document).ready(function(){
+function setAnimation(t_caffeine, t_calories, t_price) {
+    
+    // Define max values for data normalization
+    var max_caffeine = 243
+    var max_calories = 259
+    var max_price = 1.0616659
+
     var caffeine = new Chartist.Pie('.caffeine', {
 //    series: [20, 10, 30, 40]
-    series: [t_caffeine]
+    series: [t_caffeine / max_caffeine * 100]
   }, {
     donut: true,
     donutWidth: 30,
@@ -12,7 +18,7 @@ $(document).ready(function(){
   
   var calories = new Chartist.Pie('.calories', {
 //    series: [20, 10, 30, 40]
-  series: [t_calories]
+  series: [t_calories / max_calories * 100]
   }, {
     donut: true,
     donutWidth: 30,
@@ -23,34 +29,36 @@ $(document).ready(function(){
   
   var price = new Chartist.Pie('.price', {
 //    series: [20, 10, 30, 40]
-  series: [t_price]
+  series: [t_price / max_price * 100]
   }, {
     donut: true,
     donutWidth: 30,
     startAngle: 270,
     total: 200,
     showLabel: false
-  });
+  });   
+
+    // Animate respective charts
+    caffeine.on('draw', animateOnDraw)
+    calories.on('draw', animateOnDraw)
+    price.on('draw', animateOnDraw)
+}
   
-  caffeine.on('draw', animateOnDraw)
-  calories.on('draw', animateOnDraw)
-  price.on('draw', animateOnDraw)
-  
-  function animateOnDraw (data) {
+function animateOnDraw (data) {
     if(data.type === 'slice') {
       // Get the total path length in order to use for dash array animation
       var pathLength = data.element._node.getTotalLength();
-  
+
       // Set a dasharray that matches the path length as prerequisite to animate dashoffset
       data.element.attr({
         'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
       });
-  
+
       // Create animation definition while also assigning an ID to the animation for later sync usage
       var animationDefinition = {
         'stroke-dashoffset': {
           id: 'anim' + data.index,
-          dur: 1000,
+          dur: 1500,
           from: -pathLength + 'px',
           to:  '0px',
           easing: Chartist.Svg.Easing.easeOutQuint,
@@ -58,23 +66,23 @@ $(document).ready(function(){
           fill: 'freeze'
         }
       };
-  
+
       // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
       if(data.index !== 0) {
         animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
       }
-  
+
       // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
       data.element.attr({
         'stroke-dashoffset': -pathLength + 'px'
       });
-  
+
       // We can't use guided mode as the animations need to rely on setting begin manually
       // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
       data.element.animate(animationDefinition, false);
     }
-  }
-  
+}
+
   // For the sake of the example we update the chart every time it's created with a delay of 8 seconds
   // caffeine.on('created', windowTimeout())
   // calories.on('created', windowTimeout())
@@ -87,5 +95,3 @@ $(document).ready(function(){
   //   }
   //   window.__anim21278907124 = setTimeout(chart.update.bind(chart), 10000);
   // }
-
-})
--- coffee.js
+++ coffee.js
@@ -4,11 +4,6 @@ var t_calories
 var t_flavor
 var t_price
 
-// Define max values for data normalization
-var max_caffeine = 243
-var max_calories = 259
-var max_price = 1.0616659
-
 console.log(data);
 
 var coffeeSet = {
@@ -28,7 +23,7 @@ $(".bean").click(function(){
     coffee.bean = $(this).attr('data')
     console.log(coffee.bean)
     calc(coffee)
-    setAnimation()
+    setAnimation(t_caffeine, t_calories, t_price)
 })
 
 // Set method
@@ -36,7 +31,7 @@ $(".method").click(function(){
     coffee.method = $(this).attr('data')
     console.log(coffee.method)
     calc(coffee)
-    setAnimation()
+    setAnimation(t_caffeine, t_calories, t_price)
 })
 
 // Set roast
@@ -44,7 +39,7 @@ $(".roast").click(function(){
     coffee.roast = $(this).attr('data')
     console.log(coffee.roast)
     calc(coffee)
-    setAnimation()
+    setAnimation(t_caffeine, t_calories, t_price)
 
 })
 
@@ -53,7 +48,7 @@ $(".milk").click(function(){
     coffee.milk = $(this).attr('data')
     console.log(coffee.milk)
     calc(coffee)
-    setAnimation()
+    setAnimation(t_caffeine, t_calories, t_price)
 
 })
 
@@ -62,89 +57,10 @@ $(".sweetener").click(function(){
     coffee.sweetener = $(this).attr('data')
     console.log(coffee.sweetener)
     calc(coffee)
-    setAnimation()
+    setAnimation(t_caffeine, t_calories, t_price)
 
 })
 
-function setAnimation() {
-
-    var caffeine = new Chartist.Pie('.caffeine', {
-//    series: [20, 10, 30, 40]
-    series: [t_caffeine / max_caffeine * 100]
-  }, {
-    donut: true,
-    donutWidth: 30,
-    startAngle: 270,
-    total: 200,
-    showLabel: false,
-  });
-  
-  var calories = new Chartist.Pie('.calories', {
-//    series: [20, 10, 30, 40]
-  series: [t_calories / max_calories * 100]
-  }, {
-    donut: true,
-    donutWidth: 30,
-    startAngle: 270,
-    total: 200,
-    showLabel: false
-  });
-  
-  var price = new Chartist.Pie('.price', {
-//    series: [20, 10, 30, 40]
-  series: [t_price / max_price * 100]
-  }, {
-    donut: true,
-    donutWidth: 30,
-    startAngle: 270,
-    total: 200,
-    showLabel: false
-  });   
-
-    // Animate respective charts
-    caffeine.on('draw', animateOnDraw)
-    calories.on('draw', animateOnDraw)
-    price.on('draw', animateOnDraw)
-}
-  
-function animateOnDraw (data) {
-    if(data.type === 'slice') {
-      // Get the total path length in order to use for dash array animation
-      var pathLength = data.element._node.getTotalLength();
-
-      // Set a dasharray that matches the path length as prerequisite to animate dashoffset
-      data.element.attr({
-        'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-      });
-
-      // Create animation definition while also assigning an ID to the animation for later sync usage
-      var animationDefinition = {
-        'stroke-dashoffset': {
-          id: 'anim' + data.index,
-          dur: 1500,
-          from: -pathLength + 'px',
-          to:  '0px',
-          easing: Chartist.Svg.Easing.easeOutQuint,
-          // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
-          fill: 'freeze'
-        }
-      };
-
-      // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
-      if(data.index !== 0) {
-        animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-      }
-
-      // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
-      data.element.attr({
-        'stroke-dashoffset': -pathLength + 'px'
-      });
-
-      // We can't use guided mode as the animations need to rely on setting begin manually
-      // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
-      data.element.animate(animationDefinition, false);
-    }
-}
 
 // Calculate calories, caffeine, flavor and price of a given cup of coffee
 function calc(coffeeVal) {
