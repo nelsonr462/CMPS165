7e71b7758a0b81617b9414b0b625d5412cc031c1
Nelson Ramirez <nelsonr462@gmail.com>
2016-05-28 21:48:19 +0000

    fixed flag switching

--- js/animations.js
+++ js/animations.js
@@ -3,10 +3,14 @@ var moveLeft = "-=25%"
 var moveRight = "+=25%"
 
 $( ".mainCup" ).click(function() {
+  // Animate
   $( ".mainCup" ).animate({
     left: (mainCupFlag==true ? moveLeft : moveRight)
   }, 500, function() {
     // Animation complete.
-    mainCupFlag = (mainCupFlag==true ? false : true)
   });
+  
+  // Directional Flag
+  mainCupFlag = (mainCupFlag==true ? false : true)
+  
 });
