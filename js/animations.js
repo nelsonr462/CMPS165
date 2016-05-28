1294e5328feb1aba48b45050e60555fd7772008b
Nelson Ramirez <nelsonr462@gmail.com>
2016-05-28 21:44:46 +0000

    flip flop animation

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,12 @@
+var mainCupFlag = true;
+var moveLeft = "-=25%"
+var moveRight = "+=25%"
+
 $( ".mainCup" ).click(function() {
   $( ".mainCup" ).animate({
-    left: "-=25%"
+    left: (mainCupFlag==true ? moveLeft : moveRight)
   }, 500, function() {
     // Animation complete.
+    mainCupFlag = (mainCupFlag==true ? false : true)
   });
 });
