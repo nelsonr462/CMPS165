7cc3543f671b5ab733d721128b08f76b310daccf
ajanakos <ajanakos@ucsc.edu>
2016-05-28 18:59:08 -0700

    compare animation functioning

--- js/animations.js
+++ js/animations.js
@@ -1,12 +1,20 @@
 var mainCupFlag = true;
-var moveLeft = "-=25%"
-var moveRight = "+=25%"
+var moveLeft = "-=20.3%"
+var moveRight = "+=20.3%"
 
-$( ".mainCup" ).click(function() {
+
+$( "#compare" ).click(function() {
+  $("#compare").prop('disabled', true);    
   $( ".mainCup" ).animate({
     left: (mainCupFlag==true ? moveLeft : moveRight)
   }, 500, function() {
     // Animation complete.
     mainCupFlag = (mainCupFlag==true ? false : true)
+    $("#compare").prop('disabled', false);
   });
+});
+
+$("#compare").click(function () {
+    this.toggle = !this.toggle;
+    $(".compareCup").stop().fadeTo(400, this.toggle ? 1 : 0);
 });
